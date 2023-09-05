e19bfb83e9be795e412ea9cc3129a39030dc51d3
Alvaro Eduardo Menegon Rosario <104403541+alvaromenegon@users.noreply.github.com>
2023-09-05 16:35:05 -0300

    Alterado para Next.js

--- README.md
+++ README.md
@@ -2,6 +2,12 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
+```bash
+npm install
+```
+
+para instalar as dependências
+
 First, run the development server:
 
 ```bash
@@ -17,6 +23,7 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
 
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+
 
 ## Learn More
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,10 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <a style={{textDecoration:'underline'}} href="/login" target="_blank" >Login</a>
+      <p>Edite src/app/page.js para alterar esta página</p>
 
       <div className={styles.grid}>
         <a
